6f94fffa5891ed632537afbe5db23a7b45329e71
Alex E <alex@edgeimpulse.com>
2024-07-12 12:40:55 -0700

    qemu includes factorial call

--- qemu/serial_print.c
+++ qemu/serial_print.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
 /* We'll be using the serial interface to print on the console (qemu)
- * For this, we write to the UART. 
+ * For this, we write to the UART.
  * Refer: http://www.ti.com/lit/ds/symlink/lm3s6965.pdf Table 2-8
  * for the mapping of UART devices (including UART0)
  */
@@ -12,12 +12,15 @@ volatile uint8_t* lm3s6965_uart0 = (uint8_t*)0x4000C000;
 
 void uart0_print(const char* msg);
 
+extern int factorial(int x);
+
 void main(void)
 {
+    int y = factorial(4);
     const char *start_msg = "Hello, World\n";
 
     uart0_print(start_msg);
-    
+
     while(1);
 }
 
