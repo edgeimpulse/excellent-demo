1e1da34bbde6a6713e7cd2eeba14ffd4d9a60cb9
Alex E <alex@edgeimpulse.com>
2024-08-05 15:07:19 -0700

    print with %i

--- factorial/main.c
+++ factorial/main.c
@@ -5,15 +5,7 @@ extern int factorial(int x);
 void main(void)
 {
     int y = factorial(3);
-    char *start_msg = "Factorial: ";
-    char out[10];
-    // snprintf(out, 10, "%i", y);
-
-    printf(start_msg);
-    out[0] = y + 0x30;
-    out[1] = 0;
-    printf(out);
-    printf("\n");
+    printf("Factorial pf: %i", y);
 
     while(1);
 }
