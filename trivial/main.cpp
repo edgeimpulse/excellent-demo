ee49bae38a9854093940f6fba278c75e32f2579e
Alex E <alex@edgeimpulse.com>
2025-01-16 11:20:21 -0800

    test inputs as args

--- trivial/main.cpp
+++ trivial/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdint.h>
 #include "lib_tracked.h"
+#include <vector>
 
 typedef struct {
     uint64_t shape;
@@ -11,20 +12,56 @@ typedef struct {
 
 extern "C" void my_add(ndarray *a, ndarray *b, ndarray *out);
 
-int main(void)
+// Helper function modified to return a vector
+std::vector<float> parse_numbers(const char* str) {
+    std::vector<float> result;
+    std::string num;
+
+    while (*str) {
+        if (*str == ',') {
+            result.push_back(std::stof(num));
+            num.clear();
+        } else {
+            num += *str;
+        }
+        str++;
+    }
+    if (!num.empty()) {
+        result.push_back(std::stof(num));
+    }
+    return result;
+}
+
+int main(int argc, char* argv[])
 {
-    float data[10000] = {1,2,3,4};
-    ndarray in = {4,4,data};
-    ndarray in2 = {4,4,data};
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " num1,num2,num3,... num1,num2,num3,...\n";
+        return 1;
+    }
+
+    std::vector<float> data1 = parse_numbers(argv[1]);
+    std::vector<float> data2 = parse_numbers(argv[2]);
+
+    if (data1.size() != data2.size()) {
+        std::cerr << "Error: Input arrays must be the same length\n";
+        return 1;
+    }
+
+    ndarray in = {data1.size(), sizeof(float), data1.data()};
+    ndarray in2 = {data2.size(), sizeof(float), data2.data()};
     ndarray out;
     my_add(&in, &in2, &out);
 
     std::cout << "out: shape=" << out.shape
               << ", stride=" << out.stride
-              << ", data=[" << out.data[0] << ", "
-              << out.data[1] << ", "
-              << out.data[2] << ", "
-              << out.data[3] << "]\n";
+              << ", data=[";
+    for (uint64_t i = 0; i < out.shape; i++) {
+        std::cout << out.data[i];
+        if (i < out.shape - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]\n";
     std::cout << "Max allocation: " << seq_max_allocation() << " bytes" << std::endl;
     return 0;
 }
